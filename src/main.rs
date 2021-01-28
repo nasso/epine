658eca32529f0425f0ddeafd752e9747f09264e8
nasso <nassomails@gmail.com>
2021-01-28 18:08:30 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    println!("Generating Makefi-...");
+    println!("Whoops! I'm not supposed to have released publicly yet...");
+    println!("You will have to write your Makefile yourself for now!");
+}
